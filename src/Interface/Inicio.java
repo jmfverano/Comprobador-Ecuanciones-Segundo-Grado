32a0d3f0820a906a83bfd39b72f4ff3bb6d562cb
jmfverano <josemanuel.tecnico@gmail.com>
2012-09-02 15:03:44 +0200

    Inicio del proyecto, creación de clases y gui.

---
+++ src/Interface/Inicio.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interface;
+
+/**
+ *
+ * @author Xhaman
+ */
+public class Inicio {
+    
+    GUI_Principal gui;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        GUI_Principal gui = new GUI_Principal();
+        gui.setVisible(true);
+    }
+}
